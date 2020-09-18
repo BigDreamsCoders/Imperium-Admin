8bceb29a602cf79d63beefde8e3c7818dc46fcb9
petrlr14 <00057616@uca.edu.sv>
2020-09-18 01:57:42 -0600

    Add: basic configuration with essential libraries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 const AppWithRouter = () => (
   <Router basename={`${process.env.PUBLIC_URL}`}>
     <App />
+    <ReactQueryDevtools />
   </Router>
 );
 
-ReactDOM.render(<AppWithRouter />, document.getElementById("root"));
+ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
