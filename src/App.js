21c179431130ea8eaaf1cb18a2c7146884b31857
petrlr14 <pedrodeveloper14@gmail.com>
2020-09-23 19:58:05 -0600

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'antd';
 
 export const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ReactQueryDevtools } from 'react-query-devtools';
+//import { ReactQueryDevtools } from 'react-query-devtools';
 import './index.css';
 import './assets/main.css';
 
