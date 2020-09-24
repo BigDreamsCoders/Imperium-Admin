cfaa47b5e6f63997661b7c9724c1205ec11fa59a
petrlr14 <pedrodeveloper14@gmail.com>
2020-09-23 19:53:46 -0600

    add antd and tailwind basic configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Button } from 'antd';
 
 export const App = () => {
-  return <h1>Start working, you slooth!</h1>;
+  return (
+    <div className=' h-screen flex justify-center items-center bg-gray-200'>
+      <span className='font-bold text-2xl text-center text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-500'>
+        Start working, you slooth!
+      </span>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactQueryDevtools } from 'react-query-devtools';
+import './index.css';
+import './assets/main.css';
 
-const AppWithRouter = () => (
-  <Router basename={`${process.env.PUBLIC_URL}`}>
-    <App />
-    <ReactQueryDevtools />
-  </Router>
-);
+const AppWithRouter = () => {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  );
+};
 
 ReactDOM.render(<AppWithRouter />, document.getElementById('root'));
